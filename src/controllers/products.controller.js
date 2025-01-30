8f9962224b9e8c8556ef7f501c16ed990f579a77
Jose Francisco Flores Amador <230417@utxicotepec.edu.mx>
2025-01-30 17:13:06 -0600

    Practica
    Guardando cambios

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,101 +2,98 @@ import { request, response } from "express";
 import productDao from "../dao/products.dao.js";
 import { error } from "console";
 
-const productsController={};
+const productsController = {};
 
-productsController.getAll=(request,response)=>{
-    //aqui se piden los productos al DAO porcesa la peticion
+productsController.getAll = (request, response) => {
     productDao.getAll()
-    .then((products)=>{
-        response.render('../src/views/index.ejs')
-        // response.json({
-        //     data:products
-        // })
-    })
-    .catch((error)=>{
-        response.json({
-            data:{
-                "message":error
-            }
+        .then((products) => {
+            response.render('../src/views/index.ejs', { products: products }); // Asegúrate de pasar la variable products
         })
-    });
-};
-//para mostrar solo uno
-productsController.getOne=(request,response)=>{
-    productDao.getOne(request.params.barcode)//ESTO QUE ESTA EN PARENTESIS SE LLAMA ARGUMENTO DE LLAMADA 
-    .then((product)=>{
-        if(product!=null)
-            response.json({data:product});
-        else
-            response.json({data:{message:"Product not fund"}});
-        }
-    )
-    .catch((error)=>{
-        response.json({
-            data:{
-                message:error
-            }
+        .catch((error) => {
+            response.json({
+                data: {
+                    message: error
+                }
+            });
         });
-    }
-)};
-//funcion que:
-productsController.insert=(request,response)=>{
-    productDao.insert(request.body)
-    .then((product)=>{
-        response.json({
-            
-            // data:{
-            //     message:"Product insert successefully",
-            //     product:product
-            // }
-            
-        })
-        res.redirect('/groceries/products/getAll')
-    })
-    .catch((error)=>{
-        response.json({
-            data:{
-                message:error
-            }
-        })
-    });
-};
-productsController.updateOne=(request,response)=>{
-    productDao.updateOne(request.body,request.params.barcode)
-    .then((result)=>{
-        response.json({
-            data:{
-                message:"product updated successfully",
-                result:result
-            }
-        })
-    })
-    .catch((error)=>{
-        response.json({
-            data:{
-                error: error
-            }
-        })
-    });
 };
 
-productsController.deleteOne=(request,response)=>{
+// para mostrar solo uno
+productsController.getOne = (request, response) => {
+    productDao.getOne(request.params.barcode)
+        .then((product) => {
+            if (product != null) {
+                return response.json({ data: product });
+            } else {
+                return response.json({ data: { message: "Product not found" } });
+            }
+        })
+        .catch((error) => {
+            return response.json({
+                data: {
+                    message: error
+                }
+            });
+        });
+};
+
+// función que:
+productsController.insert = (request, response) => {
+    productDao.insert(request.body)
+        .then((product) => {
+            response.json({
+                // data: {
+                //     message: "Product insert successfully",
+                //     product: product
+                // }
+            });
+            // return response.redirect('/groceries/products/getAll'); // Eliminado para evitar el error
+        })
+        .catch((error) => {
+            return response.json({
+                data: {
+                    message: error
+                }
+            });
+        });
+};
+
+productsController.updateOne = (request, response) => {
+    productDao.updateOne(request.body, request.params.barcode)
+        .then((result) => {
+            return response.json({
+                data: {
+                    message: "Product updated successfully",
+                    result: result
+                }
+            });
+        })
+        .catch((error) => {
+            return response.json({
+                data: {
+                    error: error
+                }
+            });
+        });
+};
+
+productsController.deleteOne = (request, response) => {
     productDao.deleteOne(request.params.barcode)
-    .then((productDelete)=>{
-        response.json({
-        data:{
-            message:"Product deleted successfully",
-            product_delete:productDelete
-            }
+        .then((productDelete) => {
+            return response.json({
+                data: {
+                    message: "Product deleted successfully",
+                    product_delete: productDelete
+                }
+            });
         })
-    })
-    .catch((error)=>{
-        response.json({
-            data:{
-                error: error
-            }
-        })
-    });
-}
+        .catch((error) => {
+            return response.json({
+                data: {
+                    error: error
+                }
+            });
+        });
+};
 
 export default productsController;
